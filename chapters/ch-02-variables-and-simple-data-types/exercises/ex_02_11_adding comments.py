d94b7c6f6869ddac31b3f3e807add55ecad37695
Philip Alegret <philip.alegret@gmail.com>
2025-04-08 09:12:55 -0400

    Added remaining chapter 2 exercises.

---
+++ chapters/ch-02-variables-and-simple-data-types/exercises/ex_02_11_adding comments.py
@@ -0,0 +1,21 @@
+# Phil Alegret
+# 4/8/2025
+# The Zen of Python, by Tim Peters
+import this
+
+
+# Phil Alegret
+# 4/8/2025
+# The following lines of code perform basic arithmetic operations.
+print(4+4)
+print(12-4)
+print(4*2)
+print(16/2)
+
+
+# Phil Alegret
+# 4/8/2025
+# The following lines of code stores and prints a favorite number variable.
+favorite_number = 7
+print(f"My favorite number is {favorite_number}.")
+
