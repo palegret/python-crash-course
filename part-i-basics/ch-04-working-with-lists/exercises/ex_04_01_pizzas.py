bc2abae9541c3f45db208232aaee26613c784f89
Philip Alegret <philip.alegret@gmail.com>
2025-04-22 18:33:20 -0400

    Chapter 4 practice and exercises, part 1.

---
+++ part-i-basics/ch-04-working-with-lists/exercises/ex_04_01_pizzas.py
@@ -0,0 +1,11 @@
+favorite_pizzas = ["pepperoni", "hawaiian", "veggie"]
+
+for pizza in favorite_pizzas:
+    print(pizza)
+
+print("\n")
+
+for pizza in favorite_pizzas:
+    print(f"I really love {pizza} pizza!")
+
+print("\nI really love pizza!")
